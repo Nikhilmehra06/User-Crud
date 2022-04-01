5183eef60390fd5181614942057d2b79f160de01
Nikhil <Nikhilmehra7919@gmail.com>
2022-04-01 08:47:45 +0530

    Composing With Dev tools

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from './reducer/userReducer';
 
-const store = createStore(userReducer, composeWithDevTools());
+const store = createStore(userReducer, composeWithDevTools(applyMiddleware()));
 
 export default store;
